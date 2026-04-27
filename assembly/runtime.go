a8b41dfe895d6f2247160139537930101a769f85
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-27 14:41:43 +0800

    ♻️ (assembly): Re-enable fallback FFI connect path in runtime init

--- assembly/runtime.go
+++ assembly/runtime.go
@@ -36,7 +36,7 @@ func (a *Assembly) Init(ctx context.Context) error {
 		return err
 	}
 
-	if a.opts.sidecarAddress != "" && a.ffiClient != nil && ffi.NativeBindingEnabled() {
+	if a.opts.sidecarAddress != "" && a.ffiClient != nil {
 		if err := a.ffiClient.Connect(a.opts.sidecarAddress); err == nil {
 			return nil
 		}
