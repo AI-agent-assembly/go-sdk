2565afaa4ab2fff2fa829903f5e1b1f906f30f8d
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-27 09:24:53 +0800

    ✨ (assembly): Add WrapTools abstraction helper

--- assembly/wrap_tools.go
+++ assembly/wrap_tools.go
@@ -1,1 +1,18 @@
 package assembly
+
+// WrapTools wraps all tools with AssemblyTool governance interception.
+func WrapTools(toolList []Tool, client GovernanceClient, options ...Option) []Tool {
+	runtimeOpts := defaultRuntimeOptions()
+	for _, option := range options {
+		if option != nil {
+			option(&runtimeOpts)
+		}
+	}
+
+	wrapped := make([]Tool, len(toolList))
+	for index, tool := range toolList {
+		wrapped[index] = NewAssemblyTool(tool, client, runtimeOpts)
+	}
+
+	return wrapped
+}
