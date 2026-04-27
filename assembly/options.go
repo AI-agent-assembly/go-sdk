07e56f9b6ec781e77ef6748e041b16f462b4db7c
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-27 09:20:22 +0800

    ✨ (assembly): Add Option abstraction type

--- assembly/options.go
+++ assembly/options.go
@@ -1,1 +1,14 @@
 package assembly
+
+import "time"
+
+// Option mutates runtime options during Assembly construction.
+type Option func(*runtimeOptions)
+
+type runtimeOptions struct {
+	gatewayURL    string
+	apiKey        string
+	failClosed    bool
+	timeout       time.Duration
+	sidecarAddress string
+}
