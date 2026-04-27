5f7c17e830cdc8b7b4cbf02347fc62b7c4b6a5ab
Chisanan232 <chi10211201@cycu.org.tw>
2026-04-27 09:19:50 +0800

    ✨ (assembly): Add Assembly.Init abstraction method

--- assembly/runtime.go
+++ assembly/runtime.go
@@ -1,4 +1,12 @@
 package assembly
 
+import "context"
+
 // Assembly is the runtime entrypoint for governance-enabled execution.
 type Assembly struct{}
+
+// Init boots the runtime and prepares governance integrations.
+func (a *Assembly) Init(ctx context.Context) error {
+	_ = ctx
+	return nil
+}
